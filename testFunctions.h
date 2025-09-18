4ff13d0118f782425954fdb3d041fdb5c49ab916
Tony <talperov10@gmail.com>
2025-09-17 21:29:03 -0700

    Create testFunctions.h

---
+++ testFunctions.h
@@ -0,0 +1,12 @@
+#ifndef TESTFUNCTIONS_H
+#define TESTFUNCTIONS_H
+
+#include "dmm.h"
+
+void test_insert_feature(void);
+
+void test_delete_feature(void);
+
+void test_shuffle_feature(void);
+
+#endif
